85dcc03a9d774738e8a9e6c08b5aa239760ab8f5
priyanshigoel <75495663+priyanshigoel@users.noreply.github.com>
2022-07-30 14:57:29 +0530

    Create Neon number.cpp

---
+++ Neon number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int x,a=0,y,z,r,count=0;
+    cin>>x;
+    y=x,z=x;
+    while(x!=0) {
+        count++;
+        x=x/10;
+    }
+    while(y!=0) {
+        r=y%10;
+        a=a+pow(r,count);
+        y=y/10;
+    }
+    if(a==z)
+        cout<<"armstrong no";
+    else
+        cout<<"not an armstrong no.";
+}
