a2b26b2498c61f5ee5fe188313c3ccf54b701b75
priyanshigoel <75495663+priyanshigoel@users.noreply.github.com>
2022-08-05 22:48:21 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a<<endl<<b<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<d<<endl;
+    cout<<fixed<<setprecision(9)<<e<<endl;
+    // Complete the code.
+    return 0;
+}
