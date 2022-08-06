15fd4bf33465fa04df74f60c8c748bfea86404ef
priyanshigoel <75495663+priyanshigoel@users.noreply.github.com>
2022-08-06 11:17:18 +0530

    Create Even or odd number.cpp

---
+++ Even or odd number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int num;
+        cout<<"\n Enter Number : ";
+        cin>>num;
+        if(num%2==0)
+        {
+                cout<<"\n Even Number";
+        }
+        else
+        {
+                cout<<"\n Odd Number";
+        }
+        return 0;
+}
