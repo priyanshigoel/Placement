74357046dadf629d380161f7b2186b40b70b5852
priyanshigoel <75495663+priyanshigoel@users.noreply.github.com>
2022-08-06 11:22:58 +0530

    Create Print 1D Array.cpp

---
+++ Print 1D Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int arr[50], num, i;
+        cout<<"\n How Many Elements You Want to Store into an Array? \n";
+        cin>>num;
+        cout<<"\n Enter "<<num<<" Elements to Store into an Array : \n";
+        for(i=0; i<num; i++)
+        {
+                cin>>arr[i];
+        }
+        cout<<"\n The Elements in the Array are : \n";
+        for(i=0; i<num; i++)
+        {
+                cout<<arr[i]<<"\t";
+        }
+          return 0;
+}
