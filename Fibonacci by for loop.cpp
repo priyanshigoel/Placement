4ba418c14ce9d762bc544e8e2b5867fa17755f00
priyanshigoel <75495663+priyanshigoel@users.noreply.github.com>
2022-08-06 11:00:39 +0530

    Create Fibonacci by for loop.cpp

---
+++ Fibonacci by for loop.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+ 
+void main()
+{
+    int n,i,f,f1=0,f2=1;
+    clrscr();
+     
+    cout<<"  Enter The Number:";
+    cin>>n;
+     
+    cout<<"  The Fibonacci Series is:";
+    cout<<"  \n"<<f1<<"  \n"<<f2;
+     
+    for(i=2;i<n;i++)
+    {
+        f=f1+f2;
+        f1=f2;
+        f2=f;
+        cout<<"  \n"<<f;
+    }
+}
