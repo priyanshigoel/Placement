0bbf143a80efb6a07461363fe797cfba2019f193
priyanshigoel <75495663+priyanshigoel@users.noreply.github.com>
2022-08-10 14:12:14 +0530

    Create simple number pattern by while loop.cpp

---
+++ simple number pattern by while loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ int main()
+{
+    int i = 1, j = 0, n = 5;
+    int n = 1;
+        while (j <= i - 1) {
+            cout <<n<< " ";
+            j++;
+            n++;
+        }
+       j = 0;
+        i++;
+        cout << endl;
+    }
+    return 0;
+}
