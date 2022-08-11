c954a7c234aa291adc77587c6c1048c9c33f68ea
priyanshigoel <75495663+priyanshigoel@users.noreply.github.com>
2022-08-11 18:58:56 +0530

    Create Character pattern7.cpp

---
+++ Character pattern7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+  int i,j;
+  int n;
+  cin>>n;
+   for(i=1;i<=n;i++)
+     {
+         for(j=1;j<=i;j++)
+         {
+             cout<<((char)(n-i+1+64));
+         }
+ 
+         cout<<endl;
+     }
+ 
+  return 0;
+}
